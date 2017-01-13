a8fe7435b464d57453edceac3299925004f41742
Shreddy5150 <navarro.heron@gmail.com>
2017-01-13 14:18:05 -0800

    lab two meets minimum requirements

--- labtwo/labtwo.py
+++ labtwo/labtwo.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+import random
 
 W = 900
 H = 700
@@ -11,10 +12,109 @@ BRIGHT_RED = (255,0,0)
 BLUE = (0,0,255)
 GREEN = (0,210,20)
 BRIGHT_GREEN= (20,255,0)
+SILVER = (132,132,132)
+
+pscore = 0
+cscore = 0
+
 
 def text_objects(text,font):
     textSurface = font.render(text, True, WHITE)
     return textSurface, textSurface.get_rect()
+
+def computer_choice():
+    computerChoice = random.choice(choices)
+    return computerChoice
+
+def machine_comp(playerChoice,computerChoice):
+    rbeats = ["Scissors","Lizard"]
+    pbeats = ["Rock","Spock"]
+    scbeats = ["Paper","Lizard"]
+    lbeats = ["Spock","Paper"]
+    spbeats = ["Rock","Scissors"]
+
+    if playerChoice != computerChoice:
+
+        if (playerChoice == "Rock") and (computerChoice == rbeats[0] or computerChoice == rbeats[1]):
+            winner = "Player wins: Rock defeats %s" % (computerChoice)
+            won = "Player"
+            score_update(won)
+            return winner
+        
+        elif (playerChoice == "Paper") and (computerChoice == pbeats[0] or computerChoice == pbeats[1]):
+            winner = "Player wins: Paper defeats %s" % (computerChoice)
+            won = "Player"
+            score_update(won)
+            return winner
+
+        elif (playerChoice == "Scissors") and (computerChoice == scbeats[0] or computerChoice == scbeats[1]):
+            winner = "Player wins: Scissors defeats %s" % (computerChoice)
+            won = "Player"
+            score_update(won)
+            return winner
+
+        elif (playerChoice == "Lizard") and (computerChoice == lbeats[0] or computerChoice == lbeats[1]):
+            winner = "Player wins: Lizard defeats %s" % (computerChoice)
+            won = "Player"
+            score_update(won)
+            return winner
+        elif (playerChoice == "Spock") and (computerChoice == spbeats[0] or computerChoice == spbeats[1]):
+            winner = "Player wins: Spock defeats %s" % (computerChoice)
+            won = "Player"
+            score_update(won)
+            return winner
+
+       #####################################    Computer Wins           #################################
+       
+        elif (computerChoice == "Rock") and (playerChoice == rbeats[0] or playerChoice == rbeats[1]):
+            winner = "Computer wins: rock defeats %s" % (playerChoice)
+            won = "Computer"
+            score_update(won)
+            return winner
+
+        elif (computerChoice == "Paper") and (playerChoice == pbeats[0] or playerChoice == pbeats[1]):
+            winner = "Computer wins: paper defeats %s" % (playerChoice)
+            won = "Computer"
+            score_update(won)
+            return winner
+
+        elif (computerChoice == "Scissors") and (playerChoice == scbeats[0] or playerChoice == scbeats[1]):
+            winner = "Computer wins: scissors defeats %s" % (playerChoice)
+            won = "Computer"
+            score_update(won)
+            return winner
+
+        elif (computerChoice == "Lizard") and (playerChoice == lbeats[0] or playerChoice == lbeats[1]):
+            winner = "Computer wins: lizard defeats %s" % (playerChoice)
+            won = "Computer"
+            score_update(won)
+            return winner
+
+        elif (computerChoice == "Spock") and (playerChoice == spbeats[0] or playerChoice == spbeats[1]):
+            winner = "Computer wins: spock defeats %s" % (playerChoice)
+            won = "Computer"
+            score_update(won)
+            return winner
+
+        else:
+            pass
+
+    else:
+        winner = "Tie! Try Again."
+        return winner
+    
+
+def score_update(won):
+    global pscore
+    global cscore
+    if won == "Player":
+        pscore += 1
+        print pscore
+    elif won == "Computer":
+        cscore += 1
+        print cscore
+    else: 
+        pass
 
 if __name__ == "__main__":
     pygame.init()
@@ -36,12 +136,19 @@ if __name__ == "__main__":
 
     Background = 1
 
+    #image loads:
+    rock = pygame.image.load("rock.png")
+    paper = pygame.image.load("paper.png")
+    scissors = pygame.image.load("scissors.png")
+    lizard = pygame.image.load("lizard.png")
+    spock = pygame.image.load("spock.png")
 
     run = True 
    
     playerChoice = ""
     computerChoice = ""
-    choices = ["Rock","Paper","Scissors","Lizzard","Spock"]
+    
+    choices = ["Rock","Paper","Scissors","Lizard","Spock"]
     
     while run:
         
@@ -60,10 +167,11 @@ if __name__ == "__main__":
                 mouseclick = True
 
             if Background == 1:
+                background.fill(WHITE)
                 atom = pygame.image.load("atom.png")
                 menuText1 = menufont.render("ROCK, PAPER,", True, BLACK)
                 menuText2 = menufont.render("SCISSORS,", True, BANGRED)
-                menuText3 = menufont.render("LIZZARD, SPOCK?", True, BLACK)
+                menuText3 = menufont.render("LIZARD, SPOCK?", True, BLACK)
                 background.blit(atom,(440,0))
                 background.blit(menuText1, [W/2-240,60])
                 background.blit(menuText2, [W/2-180,125])
@@ -99,23 +207,18 @@ if __name__ == "__main__":
 
                 if 418 + 70 > mouse[0] > 418 and 205 + 70 > mouse[1] > 205 and mouseclick:
                     playerChoice = choices[4]
-                    print "%s Clicked" % (playerChoice)
 
                 elif 534 + 70 > mouse[0] > 534 and 286 + 70 > mouse[1] > 286 and mouseclick:
                     playerChoice = choices[2]
-                    print "%s Clicked" % (playerChoice)
                 
                 elif 497 + 70 > mouse[0] > 497 and 425 + 70 > mouse[1] > 425 and mouseclick:
                     playerChoice = choices[1]
-                    print "%s Clicked" % (playerChoice)
                 
                 elif 311 + 70 > mouse[0] > 311 and 287 + 70 > mouse[1] > 287 and mouseclick:
                     playerChoice = choices[3]
-                    print "%s Clicked" % (playerChoice)
 
                 elif 347 + 70 > mouse[0] > 347 and 423 + 70 > mouse[1] > 423 and mouseclick:
                     playerChoice = choices[0]
-                    print "%s Clicked" % (playerChoice)
                 else: 
                     pass
 
@@ -134,6 +237,8 @@ if __name__ == "__main__":
                         cont = pygame.draw.rect(background, BRIGHT_GREEN,(700,620,120,50))
                         if mouseclick:
                             Background += 1
+                            computerChoice = computer_choice()
+                            winner = machine_comp(playerChoice,computerChoice)
                     else:    
                         cont = pygame.draw.rect(background, GREEN,(700,620,120,50))
 
@@ -142,17 +247,76 @@ if __name__ == "__main__":
                     background.blit(textSurf, textRect)
                 else: 
                     pass
-
+                
             if Background == 3:
                 background.fill(WHITE)
 
+                pselect = menufont.render("Player: %s" % (pscore), True, BANGRED)
+                
+                if playerChoice == choices[0]:
+                    background.blit(rock, (330,70))
+                elif playerChoice == choices[1]:
+                    background.blit(paper, (330,70))
+                elif playerChoice == choices[2]:
+                    background.blit(scissors,(330,70))
+                elif playerChoice == choices[3]:
+                    background.blit(lizard, (330,70))
+                elif playerChoice == choices[4]:
+                    background.blit(spock, (330,70))
+                
                 vs = menufont.render("VS", True, BLACK)
+                
+                cselect = menufont.render("Computer: %s" % (cscore), True, BANGRED)
+                
+                if computerChoice == choices[0]:
+                    background.blit(rock, (330,435))
+                elif computerChoice == choices[1]:
+                    background.blit(paper, (330,435))
+                elif computerChoice == choices[2]:
+                    background.blit(scissors, (330,435))
+                elif computerChoice == choices[3]:
+                    background.blit(lizard, (330,435))
+                elif computerChoice == choices[4]:
+                    background.blit(spock, (330,435))
+                else:
+                    pass
+                
+                wintext = smallText.render(winner, True, BLACK)
 
-                background.blit(vs,(W/2-30,H/2-50))               
+                if 10 + 100 > mouse[0] > 10 and 10 + 50 > mouse[1] > 10:
+                    cont = pygame.draw.rect(background, BRIGHT_RED,(10,10,120,50))
+                    if mouseclick:
+                        playerChoice = ""
+                        computerChoice = ""
+                        Background = 1
+                else:    
+                    cont = pygame.draw.rect(background, BANGRED,(10,10,120,50))
+
+                textSurf, textRect = text_objects("Again!", smallText)
+                textRect.center = ((10+(120/2)), (10+(50/2)))
+                background.blit(textSurf, textRect)
+
+                if 770 + 100 > mouse[0] > 770 and 10 + 50 > mouse[1] > 10:
+                    cont = pygame.draw.rect(background, SILVER,(770,10,120,50))
+                    if mouseclick:
+                        run = False
+                else:    
+                    cont = pygame.draw.rect(background, BLACK,(770,10,120,50))
+
+                textSurf, textRect = text_objects("Quit!", smallText)
+                textRect.center = ((770+(120/2)), (10+(50/2)))
+                background.blit(textSurf, textRect)
 
 
+                background.blit(pselect,(330,(-25)))
+                background.blit(vs,(410,(H/3+40)))
+                background.blit(cselect,(260,340))
 
-
+                
+                if winner != "Tie! Try Again.": 
+                    background.blit(wintext,(240,660))
+                else:         
+                    background.blit(wintext,(320,660))
 
         pygame.display.flip()
 
